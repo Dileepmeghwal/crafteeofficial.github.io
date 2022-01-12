9efa399a110c592a974185259c19736ce9380093
Dileep Meghwal <dileepmeghwalrock275@gmail.com>
2022-01-12 15:08:02 +0530

    back button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,10 @@
+// ================TOP SCROLLING BUTTON===================
 
+// Get the element
+let topBtn = document.querySelector(".top-scrolling");
 
+// On Click, Scroll to the page's top, replace 'smooth' with 'instant' if you don't want smooth scrolling
+topBtn.onclick = () => window.scrollTo({ top: 100px, behavior: "smooth" });
 
-/*==================== SHOW MENU ====================*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
-}
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
-}
-
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-
-
+// On scroll, Show/Hide the btn with animation
+window.onscroll = () => window.scrollY > 500 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0
